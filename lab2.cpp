523c964c059a9be6a048e3b54b5f89298b080d09
Maui2000 <84158956+Maui2000@users.noreply.github.com>
2022-04-26 14:18:32 -0500

    Add files via upload

---
+++ lab2.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+/*
+int main()
+{
+    int i,sum=0;
+    for(i=2;i<=100;i++){
+        if(i%2==0){
+            sum=sum+i;
+        }
+    }
+    cout<<"La suma de enteros pares de 2 hasta 100 es: "<<sum<<endl;
+    return 0;
+}
+*/
+/*
+int main()
+{
+    int sum=0;
+    for(i=2;i<=100;i++){
+        if(i%2==0){
+            sum=sum+i;
+        }
+    }
+}
+*/
+/*
+int main()
+{
+    int i=0, mult=0;
+    for(i=5;i<=100;i+=5){
+        cout<<i<<endl;
+        mult=mult+1;
+    }
+    cout<<"Multiplos de 5 entre 1 y 100: "<<mult<<endl;
+    return 0;
+}
+*/
+/*
+int main()
+{
+    int x,y;
+
+    cout<<"Ingrese x: "<<endl;
+    cin>>x>>;
+    cout<<"Ingrese y: "<<endl;
+    cin>>y>>;
+    if(x>y){
+        int z=x;
+        x=y;
+        y=z;
+    }
+    for(int i)
+
+    return 0;
+}
+ */
+
+int main()
+{
+    int repetidor
+
+	cout << "ingrese la cantidad de numeros a procesar: ";
+	cin >> repetidor;
+	for (; repetidor > 0;){
+		int menor=0,mayor=0,numero=0,repetidor = 0;
+		cout << "ingrese un numero:";
+		cin >> numero;
+		repetidor = repetidor -1;
+		if (numero > mayor);
+			mayor = numero;
+		if (numero < menor);
+			menor = numero;
+	}
+	cout << "El mayor numero ingresado fue: "<< mayor << endl;
+	cout << "El menor numero ingresado fue : "<< menor << endl;
+	return 0;
+}
